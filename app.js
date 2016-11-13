271c109ce02afbb4904b6e53bed336b455bcc67a
Andrew Ho <andrwho25@gmail.com>
2016-11-13 03:32:55 -0800

    Adds styles setup Inserts pre styles Adds event listeners

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 require('style!css!./basestyles.css')
+import preStyles from 'raw!./styles/prestyles.css'
+let styleText = [0].map((i) => { return require(`raw!./styles/styles${i}.css`)})
+let style, styleElement, workElement, skipAnimationElement, pauseElement
+let animationSkipped = false, paused = false, done = false
 
 const initialize = () => {
   console.log('we\'re ready to rock')
+  insertPreStyle()
   writeLinks()
+  createSelectors()
+  createEventHandlers()
+}
+
+const insertPreStyle = () => {
+  let preStyleElement = document.createElement('style')
+  preStyleElement.textContent = preStyles
+  document.head.insertBefore(preStyleElement, document.querySelector('style'))
+}
+
+const createSelectors = () => {
+  // use global selectors
+  style = document.querySelector('#style-tag')
+  styleElement = document.querySelector('#style-text')
+  workElement = document.querySelector('#work-text')
+  skipAnimationElement = document.querySelector('#skip-animation')
+  pauseElement = document.querySelector('#pause-resume')
+}
+
+const createEventHandlers = () => {
+  // allows user entry to live update style
+  styleElement.addEventListener('input', () => {
+    style.textContent = styleElement.textContent
+  })
+
+  // skip animation if clicked
+  skipAnimationElement.addEventListener('click', (e) => {
+    e.preventDefault()
+    animationSkipped = true
+  })
+
+  pauseElement.addEventListener('click', (e) => {
+    let text = pauseElement.textContent
+    e.preventDefault()
+    if (paused) {
+      text = "Paused"
+      paused = false
+    } else {
+      text = "Resume"
+      paused = true
+    }
+  })
 }
 
 const writeLinks = () => {
   let html = require('raw!./links.html')
-  let header = document.querySelector('header')
+  let header = document.querySelector('.links')
   header.innerHTML = html
 }
 
