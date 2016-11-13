105e1a7b64e8786762b4b2da8b9cb566d5ba9605
Andrew Ho <andrwho25@gmail.com>
2016-11-13 03:10:05 -0800

    Add a link section and raw-loader to import html

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('style!css!./basestyles.css')
 
 const initialize = () => {
   console.log('we\'re ready to rock')
+  writeLinks()
+}
+
+const writeLinks = () => {
+  let html = require('raw!./links.html')
+  let header = document.querySelector('header')
+  header.innerHTML = html
 }
 
 document.addEventListener('DOMContentLoaded', initialize)
